371d03cd8661598ee75110474bb8801021638015
Sadra_Zargari <sadra.zr.81@gmail.com>
2024-08-27 09:33:55 +0330

    set updating product

---
+++ tests/Feature/Product/ProductUpdateTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests\Feature\Product;
+
+use App\Models\Product\Product;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ProductUpdateTest extends TestCase
+{
+    protected $token;
+    public function setup() : void
+     {
+        parent::setUp();
+        $loginResponse = $this->postJson('api/auth/user/login', [
+            'username' => 'sadra-Zargarii',
+            'password' => 'Sadr@209111',
+        ]);
+        $this->token = $loginResponse->json('__token__');
+    }
+
+    public function test_product_update()
+    {
+        $product = Product::latest()->first();
+        $this->assertNotNull($product);
+        $data = [
+            'name' => 'proc_2',
+            'status' => 0,
+            'price' => 2222222,
+            'details' => 'proc_2_details',
+            'color' => 'black',
+            'is_sale' => 222222,
+            'count' => 2222,
+        ];
+        $response = $this->put('api/product/update/' . $product->id, $data ,[
+            'Authorization' => 'Bearer ' . $this->token,
+        ]); 
+
+        $response->assertStatus(200);
+    }
+}
